27a1da502d5d4d844e0cd6566fc8ba03ef456e53
albar4 <fikrialbar4@gmail.com>
2021-01-20 21:47:34 +0700

    first Commit

---
+++ ProjectPCD/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace ProjectPCD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.InvertionButton = new System.Windows.Forms.Button();
+            this.BlurButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.GrayscaleButton = new System.Windows.Forms.Button();
+            this.EdgeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(12, 12);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(75, 36);
+            this.OpenButton.TabIndex = 0;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(115, 12);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 36);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // InvertionButton
+            // 
+            this.InvertionButton.Location = new System.Drawing.Point(740, 64);
+            this.InvertionButton.Name = "InvertionButton";
+            this.InvertionButton.Size = new System.Drawing.Size(87, 36);
+            this.InvertionButton.TabIndex = 2;
+            this.InvertionButton.Text = "Invertion";
+            this.InvertionButton.UseVisualStyleBackColor = true;
+            // 
+            // BlurButton
+            // 
+            this.BlurButton.Location = new System.Drawing.Point(740, 116);
+            this.BlurButton.Name = "BlurButton";
+            this.BlurButton.Size = new System.Drawing.Size(87, 36);
+            this.BlurButton.TabIndex = 3;
+            this.BlurButton.Text = "Blur";
+            this.BlurButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 483);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Brightness";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(95, 480);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(398, 56);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.TickFrequency = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(506, 488);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(709, 396);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "*.BMP;*.JPG;*.GIF;*.TIF)|*.BMP;*.JPG;*.GIF;*.TIF|All files (*.*)|*.*";
+            // 
+            // GrayscaleButton
+            // 
+            this.GrayscaleButton.Location = new System.Drawing.Point(740, 170);
+            this.GrayscaleButton.Name = "GrayscaleButton";
+            this.GrayscaleButton.Size = new System.Drawing.Size(87, 36);
+            this.GrayscaleButton.TabIndex = 8;
+            this.GrayscaleButton.Text = "Grayscale";
+            this.GrayscaleButton.UseVisualStyleBackColor = true;
+            // 
+            // EdgeButton
+            // 
+            this.EdgeButton.Location = new System.Drawing.Point(740, 225);
+            this.EdgeButton.Name = "EdgeButton";
+            this.EdgeButton.Size = new System.Drawing.Size(87, 36);
+            this.EdgeButton.TabIndex = 9;
+            this.EdgeButton.Text = "Edge D";
+            this.EdgeButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 535);
+            this.Controls.Add(this.EdgeButton);
+            this.Controls.Add(this.GrayscaleButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BlurButton);
+            this.Controls.Add(this.InvertionButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.OpenButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aplikasi konversi citra sederhana";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button InvertionButton;
+        private System.Windows.Forms.Button BlurButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button GrayscaleButton;
+        private System.Windows.Forms.Button EdgeButton;
+    }
+}
+
---
+++ ProjectPCD/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectPCD
+{
+    public partial class Form1 : Form
+    {
+        Bitmap newBitmap;
+        Image file;
+        Boolean opened = false;
+        int blurAmount = 1;
+        int brightness = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label2.Text = trackBar1.Value.ToString();
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if(dr == DialogResult.OK)
+            {
+                file = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = file;
+                opened = true;
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+           
+            if (opened == true)
+            {
+                DialogResult dr = saveFileDialog1.ShowDialog();
+                if (dr == DialogResult.OK)
+            {
+             
+                    if(saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3).ToLower() == "bmp")
+                    {
+                        file.Save(saveFileDialog1.FileName, ImageFormat.Bmp);
+                    }
+                    if (saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3).ToLower() == "jpg")
+                    {
+                        file.Save(saveFileDialog1.FileName, ImageFormat.Jpeg);
+                    }
+                    if (saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3).ToLower() == "png")
+                    {
+                        file.Save(saveFileDialog1.FileName, ImageFormat.Png);
+                    }
+                    if (saveFileDialog1.FileName.Substring(saveFileDialog1.FileName.Length - 3).ToLower() == "tif")
+                    {
+                        file.Save(saveFileDialog1.FileName, ImageFormat.Tiff);
+                    }
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Gambarnya dibuka dulu yaah !");
+            }
+
+        }
+    }
+}
